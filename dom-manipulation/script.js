7cac452bf749455889c09bb94f53804bd40c5c39
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-17 12:06:52 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,7 +1,13 @@
-quote1 = {
+const quote1 = {
   text: "I fear not an army of lions led by a sheep but an army of sheeps led by a lion",
   category: "ancient history quotes",
+};
+const quotes = [quote1];
+function showRandomQuote(){
+  for(i=0;i < quotes.length;i++){
+    console.log(quotes[i]);
+  };
+};
+function createAddQuoteForm(){
+  
 }
-
-quotes = [quote1]
-
