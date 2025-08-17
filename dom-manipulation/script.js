ecaa052e4750d28a97c6b7f3a84b0d206f233b65
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-17 15:02:57 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,6 +1,6 @@
 const quote1 = {
   text: "I fear not an army of lions led by a sheep but an army of sheeps led by a lion",
-  category: "ancient history quotes",
+  category: "ancient history quotes"
 };
 const quotes = [quote1];
 function showRandomQuote(){
@@ -9,12 +9,24 @@ function showRandomQuote(){
   quoteDisplay.innerHTML = `<p>${randomQuote.text}</p><small>${randomQuote.category}</small>`;
 };
 function createAddQuoteForm(){
-  quote = {};
   Quote = document.getElementById("newQuoteText");
   Category = document.getElementById("newQuoteCategory");
   newQuote = Quote.value.trim();
-  newCategory = Quote.value.trim();
-  quote.text = newQuote;
-  quote.category = newCategory;
-  quotes.append(quote);
+  newCategory = Category.value.trim();
+  quote = {
+    text: newQuote,
+    category: newCategory
+  };
+  quotes.push(quote);
+  const quoteDisplay = document.getElementById("quoteDisplay");
+  quoteDisplay.innerHTML = "";
+
+  const quoteTextEL = document.createElement("p");
+  quoteTextEL.textContent = quote.text;
+
+  const categoryTextEL = document.createElement("small");
+  categoryEL.textContent = quote.category;
+
+  quoteDisplay.appendChild(quoteTextEL);
+  quoteDisplay.appendChild(categoryEL);
 };
