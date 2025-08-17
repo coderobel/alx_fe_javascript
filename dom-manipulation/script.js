97a38902939bd1358e6bcc66d7107a9d6c618b54
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-17 14:28:21 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -7,7 +7,14 @@ function showRandomQuote(){
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   const quoteDisplay = document.getElementById("quoteDisplay");
   quoteDisplay.innerHTML = `<p>${randomQuote.text}</p><small>${randomQuote.category}</small>`;
-}
+};
 function createAddQuoteForm(){
-  
-}
+  quote = {};
+  Quote = document.getElementById("newQuoteText");
+  Category = document.getElementById("newQuoteCategory");
+  newQuote = Quote.value.trim();
+  newCategory = Quote.value.trim();
+  quote.text = newQuote;
+  quote.category = newCategory;
+  quotes.append(quote);
+};
