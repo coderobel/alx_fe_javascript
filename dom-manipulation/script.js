68a6b1bd54bd20494e641c3de433feda0e20f9ec
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-24 10:02:47 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,9 +4,8 @@ const quote1 = {
 };
 const quotes = [quote1];
 function showRandomQuote(){
-  localStorage.getItem(quotes);
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  sessionStorage.setItem(quotes, randomQuote);
+  sessionStorage.setItem("lastquote", JSON.stringify(randomQuote));
   const quoteDisplay = document.getElementById("quoteDisplay");
   quoteDisplay.innerHTML = `<p>${randomQuote.text}</p><small>${randomQuote.category}</small>`;
 };
@@ -15,12 +14,19 @@ function createAddQuoteForm(){
   const Category = document.getElementById("newQuoteCategory");
   const newQuote = Quote.value.trim();
   const newCategory = Category.value.trim();
+
+  if(!newQuote || !newCategory) {
+    alert("please enter both a quote and a category!");
+    return;
+  }
+
   const quote = {
     text: newQuote,
     category: newCategory
   };
   quotes.push(quote);
-  localStorage.setItem(quotes, quote);
+  localStorage.setItem("quotes", JSON.stringify(quotes));
+  
   const quoteDisplay = document.getElementById("quoteDisplay");
   quoteDisplay.innerHTML = "";
 
@@ -31,12 +37,17 @@ function createAddQuoteForm(){
   categoryTextEL.textContent = quote.category;
 
   quoteDisplay.appendChild(quoteTextEL);
-  quoteDisplay.appendChild(categoryEL);
+  quoteDisplay.appendChild(categoryTextEL);
 };
 document.addEventListener("DOMContentLoaded", function (){
    const ShowNewQuoteButton = document.getElementById("newQuote");
    ShowNewQuoteButton.addEventListener("click", showRandomQuote);
+
    const Add_Quote = document.getElementById("Add-Quote");
    Add_Quote.addEventListener("click", createAddQuoteForm);
-  
+
+   const savedQuotes = JSON.parse(localStorage.getItem("quotes"));
+   if (savedQuotes) {
+    quotes.push(...savedQuotes);
+   }
 });
